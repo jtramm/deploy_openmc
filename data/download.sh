b95da865ee388c376bface0f91d886ab7d117a7d
John Tramm <john.tramm@gmail.com>
2023-09-21 14:30:52 -0500

    change rm command for XS data download to .tgz

--- data/download.sh
+++ data/download.sh
@@ -2,4 +2,4 @@
 set -x
 wget https://anl.box.com/shared/static/mru2pt591sdvlml7vwn8oof5ppv0l0kf.tgz
 tar -xzvf mru2pt591sdvlml7vwn8oof5ppv0l0kf.tgz
-rm mru2pt591sdvlml7vwn8oof5ppv0l0kf.xz
+rm mru2pt591sdvlml7vwn8oof5ppv0l0kf.tgz
